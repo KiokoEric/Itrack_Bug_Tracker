dfbf02b3a1f5bae3a01f8c1f4a7a972a97818c7c
kiokoeric <kiokoerick040@gmail.com>
2024-07-18 17:55:22 +0300

    Creation of the Create Project Page in the Itrack Website.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import SideBar from './Components/SideBar/SideBar';
+const Create_Project = React.lazy(() => import('./Pages/Projects_Folder/Create_Project/Create_Project'))
 
 function App() {
 
   return (
     <div className='flex gap-3 m-auto mt-0.5 p-2 w-full'>
       <SideBar />
+      <Routes>
+        <Route path='/Create_Project' element={ <React.Suspense><Create_Project /> </React.Suspense> } />
+      </Routes>
     </div>
   )
 }
