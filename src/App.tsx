88aba167ecbb6a4d1b2c85430403fe50559fef0f
kiokoeric <kiokoerick040@gmail.com>
2024-07-18 13:08:55 +0300

    Creation of the SideBar in the Itrack website.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import SideBar from './Components/SideBar/SideBar';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='flex gap-3 m-auto mt-0.5 p-2 w-full'>
+      <SideBar />
+    </div>
   )
 }
 
