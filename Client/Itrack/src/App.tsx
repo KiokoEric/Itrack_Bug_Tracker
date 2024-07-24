d7216d1ab233df59d25ebcbd73549cb91a5a74f5
kiokoeric <kiokoerick040@gmail.com>
2024-07-24 14:20:53 +0300

    Creation of the Delete Page in the User Section of the Itrack Web App.

--- Client/Itrack/src/App.tsx
+++ Client/Itrack/src/App.tsx
@@ -2,19 +2,20 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SideBar from './Components/SideBar/SideBar';
-const Projects = React.lazy(() => import('./Pages/Projects_Folder/Projects/Projects'))
-const Tickets = React.lazy(() => import('./Pages/Tickets_Folder/Tickets/Tickets'))
 const Login = React.lazy(() => import('./Pages/User/Login/Login'))
-const Profile = React.lazy(() => import('./Pages/User/Profile/Profile'))
 const Delete = React.lazy(() => import('./Pages/User/Delete/Delete'))
+const Profile = React.lazy(() => import('./Pages/User/Profile/Profile'))
+const Tickets = React.lazy(() => import('./Pages/Tickets_Folder/Tickets/Tickets'))
+const Projects = React.lazy(() => import('./Pages/Projects_Folder/Projects/Projects'))
 const Registration = React.lazy(() => import('./Pages/User/Registration/Registration'))
 const Administration = React.lazy(() => import('./Pages/Administration/Administration'))
-const ProjectArchives = React.lazy(() => import('./Pages/Archives_Folder/ProjectArchives/ProjectArchives'))
-const TicketArchives = React.lazy(() => import('./Pages/Archives_Folder/TicketArchives/TicketArchives'))
-const My_Projects = React.lazy(() => import('./Pages/Projects_Folder/My_Projects/My_Projects'))
 const My_Tickets = React.lazy(() => import('./Pages/Tickets_Folder/My_Tickets/My_Tickets'))
+const My_Projects = React.lazy(() => import('./Pages/Projects_Folder/My_Projects/My_Projects'))
 const Create_Ticket = React.lazy(() => import('./Pages/Tickets_Folder/Create_Ticket/Create_Ticket'))
 const Create_Project = React.lazy(() => import('./Pages/Projects_Folder/Create_Project/Create_Project'))
+const TicketArchives = React.lazy(() => import('./Pages/Archives_Folder/TicketArchives/TicketArchives'))
+const ProjectArchives = React.lazy(() => import('./Pages/Archives_Folder/ProjectArchives/ProjectArchives'))
+
 
 function App() {
 
@@ -22,19 +23,19 @@ function App() {
     <div className='flex gap-3 m-auto mt-0.5 p-2 w-full'>
       <SideBar />
       <Routes>
-        <Route path='/Create_Project' element={ <React.Suspense><Create_Project /> </React.Suspense> } />
-        <Route path='/Create_Ticket' element={ <React.Suspense><Create_Ticket /> </React.Suspense> } />
-        <Route path='/Projects' element={<React.Suspense><Projects /> </React.Suspense>} />
-        <Route path='/My_Projects' element={<React.Suspense><My_Projects /> </React.Suspense>} />
+        <Route path='/' element={<React.Suspense><Login /></React.Suspense>} />
+        <Route path='/Delete' element={<React.Suspense><Delete /></React.Suspense>} />
+        <Route path='/Profile' element={<React.Suspense><Profile /></React.Suspense>} />
         <Route path='/Tickets' element={<React.Suspense><Tickets /> </React.Suspense>} />
+        <Route path='/Projects' element={<React.Suspense><Projects /> </React.Suspense>} />
         <Route path='/My_Tickets' element={<React.Suspense><My_Tickets /> </React.Suspense>} />
+        <Route path='/My_Projects' element={<React.Suspense><My_Projects /> </React.Suspense>} />
+        <Route path='/Registration' element={<React.Suspense><Registration /></React.Suspense>} />
+        <Route path='/TicketArchives' element={<React.Suspense><TicketArchives /></React.Suspense>} />
+        <Route path='/Create_Ticket' element={ <React.Suspense><Create_Ticket /> </React.Suspense> } />
         <Route path='/Administration' element={<React.Suspense><Administration /> </React.Suspense>} />
         <Route path='/ProjectArchives' element={<React.Suspense><ProjectArchives /></React.Suspense>} />
-        <Route path='/TicketArchives' element={<React.Suspense><TicketArchives /></React.Suspense>} />
-        <Route path='/' element={<React.Suspense><Login /></React.Suspense>} />
-        <Route path='/Registration' element={<React.Suspense><Registration /></React.Suspense>} />
-        <Route path='/Profile' element={<React.Suspense><Profile /></React.Suspense>} />
-        <Route path='/Delete' element={<React.Suspense><Delete /></React.Suspense>} />
+        <Route path='/Create_Project' element={ <React.Suspense><Create_Project /> </React.Suspense> } />
       </Routes>
     </div>
   )
