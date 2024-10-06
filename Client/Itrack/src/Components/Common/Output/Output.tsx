308456af5407efa2af9248e925e196fa642be274
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-06 15:52:39 +0300

    Update of the Reusable Output Component.

--- Client/Itrack/src/Components/Common/Output/Output.tsx
+++ Client/Itrack/src/Components/Common/Output/Output.tsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { MdDelete } from "react-icons/md";
-import { FaArchive } from "react-icons/fa";
-import { MdEditDocument } from "react-icons/md";
 import ProjectImage from "../../../assets/ProjectImage.png";
-
 interface OutputProps {
-    Delete?: (e: React.MouseEvent<SVGAElement>) => void;
-    Archive?: (e: React.MouseEvent<SVGAElement>) => void;
-    ID: any;
+    ID: any
     Image: any;
     Name: string;
+    children?: any;
+    Navigate: any;
 }
 
-const Output: React.FC<OutputProps> = ({ ID, Image, Name, Delete, Archive }) => {
+const Output: React.FC<OutputProps> = ({ ID, Navigate, Image, Name, children }) => { 
 return (
     <div className='flex flex-col gap-5 px-5 text-black'>
-        <div key={ID} >
-            <figure className='flex gap-4 items-center justify-center'>
-                <Link to={`/ProjectDetails/${ID}`} className='text-black no-underline' >  
-                    {Image ? (<img src={Image} alt="" />) : (<img src={ProjectImage} alt="" />) }
+        <div key={ID}>
+            <figure className='flex flex-col gap-4 items-center justify-center'>
+                <Link to={Navigate} className='text-black no-underline' >  
+                    {Image ? (<img src={Image} alt="" className='rounded' width='320px' />) : (<img src={ProjectImage} alt="" />) }
                     <figcaption>
-                        <h2 className="text-3xl">{Name}</h2>
+                        <h2 className="font-bold mt-5 text-center text-3xl">{Name}</h2>
                     </figcaption>
                 </Link>
-                <div>
-                    <Link to={`/Project/${ID}`} key={ID} >
-                        <MdEditDocument size="1rem" className='bg-red-700' color="black" />
-                    </Link>
-                    <MdDelete size="1rem" className='bg-red-700' color="black" onClick={Delete} /> 
-                    <FaArchive size="1rem" className='bg-red-700' color="black" onClick={Archive} /> 
-                </div>
-            </figure> 
+                {children}
+            </figure>  
         </div>
     </div>
 
