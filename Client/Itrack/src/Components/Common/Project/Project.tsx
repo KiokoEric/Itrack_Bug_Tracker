9b3bc2009d372f1dc630dd142883c27f2304cb9e
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-06 15:58:59 +0300

    Update of the Reusable Project Component

--- Client/Itrack/src/Components/Common/Project/Project.tsx
+++ Client/Itrack/src/Components/Common/Project/Project.tsx
@@ -1,50 +1,34 @@
 import React from 'react';
-import Button from '../Button/Button';
-import { MdEditDocument } from "react-icons/md";
 import ProjectImage from "../../../assets/ProjectImage.png";
-
 interface ProjectProps {
-    Navigate?: (e: React.MouseEvent<HTMLDivElement>) => void;
     Image: any;
-    Name: string;
-    Assigned: any;
-    EndDate: string;
-    Manager: string;
-    Priority: string;
-    StartDate: string;
-    Description: string;
-    ContainerStyle: string;
+    Name: any;
+    EndDate: any;
+    Manager: any;
+    children: any;
+    Priority: any;
+    StartDate: any;
+    Description: any;
+    ContainerStyle?: any;
 }
 
-const Project:React.FC<ProjectProps> = ({ ContainerStyle, Image, Name, EndDate, Manager, Priority, Assigned, StartDate, Description, Navigate }) => {
+const Project:React.FC<ProjectProps> = ({ ContainerStyle, Image, Name, EndDate, Manager, Priority, StartDate, Description, children }) => {
 return (
-    <div className={ContainerStyle} >
+    <div className={ContainerStyle}>
         <figure>
-            {Image ? (<img src={Image} alt="" width="500px" />) : (<img src={ProjectImage} alt="" width="500px" />) }
+            {Image ? (<img src={Image} alt="" className='w-12/12' />) : (<img src={ProjectImage} alt="" className='w-12/12' />) }
         </figure>
-        <figcaption>
-            <h1 className='font-bold text-3xl'>{Name}</h1>
-            <p>{Description}</p>
-            <h3 className='font-bold underline'>Priority Level <p>{Priority}</p> </h3>
-            <h3 className='font-bold underline'>Timeline</h3>
-            <div className='flex gap-4'>
-                <p className='font-bold'>Start Date: <span className='font-normal'>{StartDate}</span> </p>
-                <p className='font-bold'>End Date: <span className='font-normal'>{EndDate}</span></p>
+        <figcaption className="flex flex-col gap-3 items-center mb-5 xl:items-start">
+            <h2 className='font-bold mt-3 pb-2 text-3xl text-center underline lg:text-left'>{Name}</h2>
+            <p className='text-center xl:text-left'>{Description}</p>
+            <p><b>Priority Level</b>: {Priority}</p>
+            <h3 className="font-bold flex flex-col gap-3 underline">Timeline</h3>
+            <div className="flex flex-col gap-5 sm:gap-20 sm:flex-row">
+                <p><b>Start Date</b>: {StartDate}</p>
+                <p><b>End Date</b>: {EndDate}</p>
             </div>
-            <p className='font-bold'>Project Manager: <span>{Manager}</span></p>
-            <p>Assigned Developers: {Assigned.map((Item: any) => {
-                return(
-                    <div className='flex flex-col gap-3 list-disc mt-5'>
-                        <li>{Item[0].Name}</li>
-                    </div>
-                )
-            })}</p> 
-            <Button
-                onClick={Navigate}
-                Children={<MdEditDocument />} 
-                ButtonStyle='bg-blue-700 mt-5 p-3 rounded text-white'
-                ButtonText='Edit Details'
-            />
+            <p><b>Project Manager</b>: {Manager}</p> 
+            {children}
         </figcaption>
     </div>
 )
