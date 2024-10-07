c28a2cd26e1da9054f97eb39829657ceaf5e9b7f
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-07 21:34:12 +0300

    Update of the App.js File

--- Server/App.js
+++ Server/App.js
@@ -1,13 +1,14 @@
-const app = express();
 const cors = require("cors");
 const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+const app = express(); 
+
 dotenv.config();
 
-// Middleware
+// MIDDLEWARE
 
 app.use(cors({
     origin: ["http://localhost:5173"],
@@ -17,28 +18,27 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser()) 
 
-// MongoDB Connection    
+// MONGODB CONNECTION    
 
 const dbUrl = process.env.MONGODB_URL
 
 mongoose.connect(dbUrl)
 .then(() => console.log("Connected to the database!"))
 
-    // import Routes
+    // IMPORT ROUTES
 
     const UserRoute = require("./Routes/UserRoute");
     const TicketRoute = require("./Routes/TicketRoute"); 
     const ProjectRoute = require("./Routes/ProjectRoute");
     const CommentRoute = require("./Routes/CommentRoute");  
     const ArchiveRoute = require("./Routes/ArchivedTicketsRoute");
-    const ArchivedProjectRoute = require("./Routes/ArchivedProjectsRoute");
-
+    const ArchivedProjectRoute = require("./Routes/ArchivedProjectsRoute"); 
     
     app.use("/Users", UserRoute);
-    app.use("/Tickets", TicketRoute); 
+    app.use("/Ticket", TicketRoute); 
     app.use("/Projects", ProjectRoute);
     app.use("/Comments", CommentRoute); 
     app.use("/TicketArchives", ArchiveRoute);
-    app.use("/ProjectArchives", ArchivedProjectRoute); 
+    app.use("/ProjectArchives", ArchivedProjectRoute);   
 
 app.listen(4000)      
