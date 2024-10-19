44ef5cd6f61d12b7b3a8f118c69d5e107e8d0471
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-19 21:31:35 +0300

    Update of the Reusable Table Results Component

--- Client/Itrack/src/Components/Common/Mobile_TableBody/Table_Results.tsx
+++ Client/Itrack/src/Components/Common/Mobile_TableBody/Table_Results.tsx
@@ -12,7 +12,7 @@ interface TableProps {
 
 const TableResults:React.FC<TableProps> = ({ID, Navigate, FirstOutput, SecondOutput, ThirdOutput,}) => {
 return (
-    <TableRow key={ID} sx={{ borderBottom: '1px solid black', display: 'flex', justifyContent: 'space-between', height: '45px' ,margin: 'auto', width: '340px' , overflow: 'hidden' }}>
+    <TableRow key={ID} sx={{ borderBottom: '1px solid black', display: 'flex', justifyContent: 'space-between', height: '45px' ,margin: 'auto', width: '360px' , overflow: 'hidden' }}>
         <TableCell><Typography sx={{ fontSize: 17, width: '90px'}}><Link to={Navigate} className='text-black no-underline'>{FirstOutput}</Link></Typography></TableCell>
         <TableCell><Typography sx={{ fontSize: 17, minWidth: '90px'}}><Link to={Navigate} className='text-black no-underline'>{SecondOutput}</Link></Typography></TableCell>
         <TableCell><Typography sx={{ fontSize: 17, width: '90px'}}><Link to={Navigate} className='text-black no-underline'>{ThirdOutput}</Link></Typography></TableCell>
