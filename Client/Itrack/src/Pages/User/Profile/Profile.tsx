687cba4444532ccc19d82c3ff56118c47b9641ec
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-27 19:24:14 +0300

    Update of the Profile Page

--- Client/Itrack/src/Pages/User/Profile/Profile.tsx
+++ Client/Itrack/src/Pages/User/Profile/Profile.tsx
@@ -121,13 +121,13 @@ return (
                 Value={Password}
                 Children={showPassword ? <FontAwesomeIcon icon={faEye} className="underline" onClick={handleTogglePassword} /> : <FontAwesomeIcon icon={faEyeSlash} className="underline" onClick={handleTogglePassword} />  }
             />
-            <div className='flex gap-16 mt-5'>
+            <div className='flex gap-5 mt-5'>
                 {
                     Administrator ? <Button
                     onClick={EditUser}
                     Children={<FaEdit size="1.3rem" />}
                     ButtonText='Edit Details'
-                    ButtonStyle='bg-black cursor-pointer flex items-center justify-center gap-2 mt-1 text-center text-white px-3 py-1.5 rounded w-40'
+                    ButtonStyle='bg-red-800 cursor-pointer flex items-center justify-center gap-2 mt-1 text-center text-white px-3 py-1.5 rounded w-40 hover:bg-black'
                 /> : null
                 }
                 {
@@ -135,7 +135,7 @@ return (
                         onClick={DeleteUser}
                         Children={<MdDelete size="1.4rem" />}
                         ButtonText='Delete Profile'
-                        ButtonStyle='bg-black cursor-pointer flex items-center justify-center gap-2 mt-1 text-center text-white px-3 py-1.5 rounded w-40'
+                        ButtonStyle='bg-red-800 cursor-pointer flex items-center justify-center gap-2 mt-1 text-center text-white px-3 py-1.5 rounded w-40 hover:bg-black'
                     /> : null
                 }
             </div>
