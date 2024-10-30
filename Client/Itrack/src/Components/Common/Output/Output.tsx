1364bccf70de3e681fe29d4d398c76000cefbec6
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-30 21:32:23 +0300

    Update of the Reusable Output Component

--- Client/Itrack/src/Components/Common/Output/Output.tsx
+++ Client/Itrack/src/Components/Common/Output/Output.tsx
@@ -13,9 +13,9 @@ const Output: React.FC<OutputProps> = ({ ID, Navigate, Image, Name, children }) 
 return (
     <div className='flex flex-col gap-5 px-5 text-black'>
         <div key={ID}>
-            <figure className='flex flex-col gap-4 items-center justify-center'>
+            <figure className='flex flex-col gap-4 items-center justify-center mb-8'>
                 <Link to={Navigate} className='text-black no-underline' >  
-                    {Image ? (<img src={Image} alt="" className='rounded' width='320px' />) : (<img src={ProjectImage} alt="" />) }
+                    {Image ? (<img src={Image} alt="" className='rounded' width='320px' />) : (<img src={ProjectImage} alt="" width='320px' />) }
                     <figcaption>
                         <h2 className="font-bold mt-5 text-center text-3xl">{Name}</h2>
                     </figcaption>
